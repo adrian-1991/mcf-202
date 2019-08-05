ef5c7deff03c98150aa73d43270fe26c4c306e10
adrian <adrianforestal_18@hotmail.com>
2019-08-05 15:22:32 -0500

    Script 0

---
+++ Clase 0/Clase_0.R
@@ -0,0 +1,22 @@
+# Adrian Botello
+#05/08/2019
+#clase 0
+
+# pasosbasicos ------------------------------------------------------------
+
+2+2
+a * a
+a + 5
+
+# importar datos  ---------------------------------------------------------
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+diametro
+# Medidas de tendencia central 
+mean(diametro)
+median(diametro)
+# Medidas dispersión 
+var(diametro)
+sd(diametro)
+
